11b78c3178a00c53f0d85e0d67165b0790fc6a50
Eric <eric_brand@dell.com>
2018-07-11 16:42:45 -0500

    initial commit

---
+++ Bontecou/Bontecou.Tests/Unit/WorldTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using Bontecou.Model;
+
+namespace Bontecou.Tests.Unit
+{
+    public class WorldTests
+    {
+		[Fact(DisplayName = "Can be created.")]
+		public void WorldCanBeCreated()
+		{
+			var world = new World();
+			world.ShouldNotBeNull();
+		}
+    }
+}
